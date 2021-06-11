83ea3c350fc5abff115d8dbf219abcfa50525181
Ai01 <2911403289@qq.com>
2021-06-11 16:55:56 +0800

    feat: core-render chore: read-me

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+var core_render = (...modules) => {
+  const e = React.createElement;
+
+  // 投放数据的获取
+  // 数据根据模块id来分发
+
+  class App extends React.Component {
+    render() {
+      return e('div', null, ...modules);
+    }
+  }
+
+  ReactDOM.render(e(App, null, null), document.getElementById('root'));
+};
+
 require([
   "https://unpkg.com/react@17/umd/react.production.min.js",
   "https://unpkg.com/react-dom@17/umd/react-dom.production.min.js",
@@ -6,13 +21,6 @@ require([
 ], function (...modules) {
   const A = modules[2].define.A();
   const B = modules[3].define.B();
-  const e = React.createElement;
 
-  class App extends React.Component {
-    render() {
-      return e('div', null, A, B);
-    }
-  }
-
-  ReactDOM.render(e(App, null, null), document.getElementById('root'));
+  core_render(A, B)
 });
--- src/moduleA.js
+++ src/moduleA.js
@@ -1,4 +1,7 @@
 // moduleA.js
+// 模块构建：
+// 1. 模块的依赖需要剔除部分无法全局的依赖
+// 2. 模块的libraryTarget是amd
 
 console.log('moduleA load');
 exports.define = {
