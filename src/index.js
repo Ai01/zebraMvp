fb7ff4e99ce93b2600dd06c994bfe89e5291a339
Ai01 <2911403289@qq.com>
2021-06-15 20:45:53 +0800

    chore: to do

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ var core_render = (...modules) => {
 
   // 投放数据的获取
   // 数据根据模块id来分发
+  // TODO: 如果在A里使用了C，如何实现。本质还是都生产了统一配置，然后下载下来。需要参考cmd规范的实现。在A中的import c会变成require(c).
 
   class App extends React.Component {
     render() {
